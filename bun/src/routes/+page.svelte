1628f84476ba8c344347d606c93828f3c20cc4e6
abbmarand <markusndrssn@gmail.com>
2024-04-25 21:16:24 +0200

    better UI

--- bun/src/routes/+page.svelte
+++ bun/src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import { onMount } from "svelte";
     import { dev } from "$app/environment";
     import axios, { AxiosHeaders } from "axios";
+    import { Button } from "$lib/components/ui/button";
+    import { Input } from "$lib/components/ui/input/index.js";
     let apidomain = "http://localhost:3001";
     let file: string = "";
     let content: string = "";
@@ -51,21 +53,30 @@
     });
 </script>
 
-<main class="bg-zinc-950 overflow-auto h-screen text-white">
-    hello
-    <div class="flex flex-col">
-        {#each todos as todo}
-            <div class="flex flex-row">
-                <p>
-                    {todo.file}
-                </p>
-                <p>
-                    {todo.content}
-                </p>
-            </div>
-        {/each}
+<main class="bg-zinc-950 overflow-auto h-screen text-white flex justify-center ove">
+    <div class="mt-14 ">
+        <h1 class="text-4xl">The Best todo manager</h1>
+        <p>This todo manager is build with the latest and greatest technologies including the new javascript runtime Bun. <br> Now there is not even a need for a database anymore!<br/>uhm, forgot that you might want to remove them but who cares</p>
+        
+        <form class="flex w-full max-w-sm items-center space-x-2">
+            <Input bind:value={file} type="text" class="text-black" placeholder="name" />
+            <Input bind:value={content} type="text" class="text-black" placeholder="description" />
+            <Button on:click={createtodo} type="submit">Add todo</Button>
+        </form>
+        <div class="flex flex-col text-white">
+            {#each todos as todo}
+            {#if todo.file!=="" || todo.file!==""}
+                <div class="flex flex-col p-4 my-4 border border-white rounded-md">
+                    <h3 class="text-4xl">
+                        {todo.file}
+                    </h3>
+                    <p class="text-xl">
+                        {todo.content}
+                    </p>
+                </div>
+                {/if}
+            {/each}
+        </div>
+      
     </div>
-    <input type="text" class="bg-zinc-800" bind:value={file} />
-    <input type="text" class="bg-zinc-800" bind:value={content} />
-    <button on:click={createtodo}> upload</button>
 </main>
